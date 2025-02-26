416bca1bfdd0bb58fde5001bdbe5cc528378c997
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-26 22:16:29 +0600

    homepage modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Home Page</h1>
+      <h1 className="text-4xl text-center my-5">Welcome To the Home Page</h1>
+      <p className="text-justify w-[60%] mx-auto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum et cum, eligendi velit fugit ipsa odit repellendus! Error ratione odio suscipit exercitationem tempore. Culpa deserunt architecto dignissimos ex, voluptatibus nihil.</p>
     </div>
   );
 };
